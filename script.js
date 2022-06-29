8663cbdf16a7949f4565d8def09fb75ddd7ee0f2
itszgee <geovane29miguel@gmail.com>
2022-06-29 20:48:26 -0300

    versao inicial

---
+++ script.js
@@ -0,0 +1,38 @@
+const nome = "Miguel"
+const sobrenome = "Oliveira"
+const data_nascimento = "29/09/2006"
+
+let altura = 1.75
+let numero_sapato = 48
+let idade = 15
+
+let celular = "(18) 98802-7881"
+let cor_favorita = "rosa roxo e azul"
+
+let qtde_moto = null
+let qtde_carro = null 
+
+// <- o valor
+qtde_carro = 1
+
+altura = 1.76
+
+celular = "(18) 99148-2663"
+
+// comparar/valiar/verificar Igualdade
+
+nome.trim().toUpperCase() == "Miguel" // false
+
+idade == 15 // true
+
+idade == 16
+
+idade == 15 // true
+
+(true == false) == (true == false)
+
+// igualdade estrita
+
+nome === "Miguel"
+
+// 
